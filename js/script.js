3b4e7babd7b7cc0160c79944706286b275419d4b
Josh <josue0991@icloud.com>
2017-02-23 16:04:20 -0600

    new theme

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
-$("body").scrollspy({
-  target: "#top-navi";
-});
+
+function activeLink(id) {
+	console.log(id);
+	var navLinks = document.getElementsByClassName("nav-link");
+	for(var i = 0; i < navLinks.length; i++) {
+		var navLink = navLinks[i];
+		navLink.style.color = "#E2E2E2";
+	}
+	var el = document.getElementById(id);
+	console.log(el);
+	el.style.color = "white";
+}
 
